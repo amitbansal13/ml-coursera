b469f081c9c2a465fa5ea3e3d411fd1c2295ef46
amitbansal13 <amitmohitbansal@gmail.com>
2018-12-16 17:32:51 +0530

    New

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -90,7 +90,7 @@ Theta2_grad=delta2/m;
 %               and Theta2_grad from Part 2.
 %
 
-Theta1_grad(:,2:end)=Theta1_grad(:,2:end)+(lambda/m)*Theta1(:,2:end);
+Theta1_grad(:,2:end)=Theta1_grad(:,2:end)+(lambda/m)*Theta1(:,2:end);%Regularize all except the bias
 Theta2_grad(:,2:end)=Theta2_grad(:,2:end)+(lambda/m)*Theta2(:,2:end);
 
 
