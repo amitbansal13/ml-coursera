adf10b6b215a3f705ebd8757bb7c34a28d6463a3
amitbansal13 <amitmohitbansal@gmail.com>
2018-12-19 21:18:38 +0530

    Logistic Regression - Week 3

--- machine-learning-ex2/ex2/ex2.m
+++ machine-learning-ex2/ex2/ex2.m
@@ -12,7 +12,7 @@
 %     predict.m
 %     costFunctionReg.m
 %
-%  For this exercise, you will not need to change any code in this file,
+%  For this exercise, you do not need to change any code in this file,
 %  or any other files other than those mentioned above.
 %
 
